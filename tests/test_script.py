5c2c7e4b7ceb30ec8430e8c74cdf769fed4fcaf8
KeerthanaChandrasekaran <ukeerthana14@gmail.com>
2023-06-07 14:04:46 +0530

    Update test_script.py

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,8 +1,26 @@
 import pytest
+import inspect
+import logging
+
+# setting log details
+loggerName = inspect.stack()[1][3]
+logger = logging.getLogger(loggerName)
+
+# creating log file
+fileHandler = logging.FileHandler('logReport.log')
+
+# defining the formatter and setting it for log file
+formatter = logging.Formatter(
+    "%(asctime)s : %(levelname)s : %(name)s : %(message)s")
+fileHandler.setFormatter(formatter)
+logger.addHandler(fileHandler)
+logger.setLevel(logging.INFO)
 
 def test_one():
+    logger.info('Test One Status: Success')
     assert 1 == 1
 
 
 def test_two():
+    logger.debug('Test Two')
     assert True, 'Valid Case'
